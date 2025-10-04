d688f00cefc624bb59497dd1360b2d02b2567c24
fionn mcgoldrick <fionnmc123@gmail.com>
2025-10-04 15:44:29 +0100

    add new java project called Vigenere. created /out folder in bin directory and ensured it worked as a small executable

--- CryptoConsumer/src/ie/atu/consumer/crypto/ConsumerRunner.java
+++ CryptoConsumer/src/ie/atu/consumer/crypto/ConsumerRunner.java
@@ -1,15 +1,15 @@
 package ie.atu.consumer.crypto;
 
+import ie.atu.sw.crypto.Algorithm;
 import ie.atu.sw.crypto.CypherFactory;
 import ie.atu.sw.crypto.Cypherable;
-import ie.atu.sw.crypto.symmetric.AESCypher;
 
 public class ConsumerRunner {
 	public static void main(String[] args) throws Throwable {
 		CypherFactory cf = CypherFactory.getInstance();
 		
-		Cypherable cypher = new AESCypher();
-		//Cypherable cypher = cf.getCypherable(Algorithm.AES);
+		//Cypherable cypher = new AESCypher();
+		Cypherable cypher = cf.getCypherable(Algorithm.AES);
 		byte[] s = new String("HAPPY DAYS").getBytes("UTF-8");
 		byte[] t = cypher.encrypt(s);
 		System.out.println(new String(t));
